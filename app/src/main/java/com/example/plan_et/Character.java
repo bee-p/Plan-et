f87c5c6e07911d9b37599ce2ed36a7a259dae46a
bee-p <weileenw@naver.com>
2021-10-26 14:48:32 +0900

    Fix: 캐릭터 화면/하단 네비게이션 아이콘 적용

--- app/src/main/java/com/example/plan_et/Character.java
+++ app/src/main/java/com/example/plan_et/Character.java
@@ -1,5 +1,6 @@
 package com.example.plan_et;
 
+import android.graphics.Matrix;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 public class Character extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
