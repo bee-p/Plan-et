6fe562a722c82865ee6e7d72c592cab532d03b8f
hye-on <ain0103@naver.com>
2021-11-02 03:29:22 +0900

    add:달력 구현

--- app/src/main/java/com/example/plan_et/Calender.java
+++ app/src/main/java/com/example/plan_et/Calender.java
@@ -1,19 +1,46 @@
 package com.example.plan_et;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Calender extends Fragment {
+
+import android.widget.CalendarView;
+import android.widget.TextView;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+
+public class Calender extends AppCompatActivity {
+
+    CalenderView calenderView;
+    TextView today;
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.calenderhome, container, false);
+   public void onCreate(Bundle savedlnstanceState){
+        super.onCreate(savedlnstanceState);
+        setContentView(R.layout.calenderhome);
+
+        today =findViewById(R.id.today);
+        calenderView =findViewById(R.id.qweqwe);
+        //날짜 변환
+        DateFormat formatter= new SimpleDateFormat("yyyy녀MM월dd일");
+        Date date =new Date(calenderView.getDate());
+
+        today.setText(formatter.format(date));
+
+        calenderView.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+            public void onSelectedDayChange(@NonNull CalendarView,int year, int month, int dayOfmonth){
+                String day;
+                day=year+"년"+(month+1)+"월"+dayOfmonth+"일";
+                today.setText(day);
+            }
+        });
     }
 }
